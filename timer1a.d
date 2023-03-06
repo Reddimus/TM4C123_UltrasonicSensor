0225ff7177682d33063c16043a07fd04bee26288
kesus <jesus.perez.cecs@gmail.com>
2023-03-06 13:20:24 -0800

    reorganize project files

--- timer1a.d
+++
@@ -1,2 +0,0 @@
-./timer1a.o: Timer1A.c C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
-  tm4c123gh6pm.h
--- ultrasonic_sensor.d
+++
@@ -1,3 +0,0 @@
-./ultrasonic_sensor.o: Ultrasonic_Sensor.c \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h PLL.h Timer1A.h \
-  SysTickPWM.h tm4c123gh6pm.h
